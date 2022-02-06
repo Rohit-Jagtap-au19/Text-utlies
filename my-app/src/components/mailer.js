500d3bb8a91e9cbe55b810a2e9b7d372db581b26
Rohit <prjagtap90@gmail.com>
2022-02-06 19:51:45 +0530

    Updated form

--- my-app/src/components/mailer.js
+++ my-app/src/components/mailer.js
@@ -1,7 +1,8 @@
 import emailjs from 'emailjs-com'
+import './mailer.css'
 const Mailer = () => {
     function sendEmail(e) {
-        alert("Thank you We'll be in touch soon.")
+        alert(`Thank you \n We'll be in touch soon.`)
 
         // e.preventDefault()
         emailjs.sendForm(
@@ -17,18 +18,9 @@ const Mailer = () => {
     }
     return (
         <>
-            <div className="container border"
-                style={{
-                    marginTop: "50px",
-                    width: '50%',
-                    height: '75vh',
-                    backgroundImage: `url('https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_23-2148902771.jpg?size=626&ext=jpg')`,
-                    backgroundPosition: 'center',
-                    backgroundSize: 'cover',
-                    borderRadius: '5px'
-                }}>
-                <h1 style={{ marginTop: '25px' }}>Contact Form </h1>
-                <form className="row" style={{ margin: '50px 85px 0px 0px', width: '32vw' }} onSubmit={sendEmail}>
+            <div className="container-1">
+                <h1>Contact Form </h1>
+                <form className="row"  onSubmit={sendEmail}>
                     <label>name</label>
                     <input type="text" name="name" className="form-control mt-3" />
 
